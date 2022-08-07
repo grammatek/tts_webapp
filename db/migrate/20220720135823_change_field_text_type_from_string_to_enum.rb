2a41068fef81021d9520b68aea9e97c6b3be006b
Anna Nikulásdóttir <anna@grammatek.com>
2022-08-07 22:00:50 +0000

    Dockerized application
    Signed-off-by: Anna Nikulásdóttir <anna@grammatek.com>

--- db/migrate/20220720135823_change_field_text_type_from_string_to_enum.rb
+++ db/migrate/20220720135823_change_field_text_type_from_string_to_enum.rb
@@ -1,5 +1,5 @@
 class ChangeFieldTextTypeFromStringToEnum < ActiveRecord::Migration[7.0]
   def change
-    change_column :processed_files, :text_type, :integer
+    change_column :processed_files, :text_type, 'integer USING CAST(text_type AS integer)'
   end
 end
